5dff1ee1dbe806452bd6d398f5604eaabfa000a2
Jesse Wardle <jesse.wardle@getweave.com>
2020-12-02 22:39:53 -0700

    Day 3 part 1

---
+++ 3/main.go
@@ -0,0 +1,63 @@
+package main
+
+import (
+	"bufio"
+	"flag"
+	"fmt"
+	"io"
+	"os"
+	"strings"
+)
+
+var input string
+
+func main() {
+	flag.StringVar(&input, "input", "input.txt", "Sets the input file to load")
+	flag.Parse()
+
+	f, err := os.Open(input)
+	if err != nil {
+		panic(err)
+	}
+	defer f.Close()
+
+	geology, err := readFile(f)
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println(treesInPath(geology, 3, 1))
+}
+
+func treesInPath(geology [][]string, stepX int, stepY int) (trees int) {
+	x := stepX
+	for y := stepY; y < len(geology); y += stepY {
+		location := geology[y][x]
+
+		if location == "#" {
+			trees++
+		}
+
+		x += stepX
+
+		if x >= len(geology[y]) {
+			x -= len(geology[y])
+		}
+	}
+
+	return trees
+}
+
+func readFile(r io.Reader) ([][]string, error) {
+	scanner := bufio.NewScanner(r)
+
+	geology := make([][]string, 0)
+	for scanner.Scan() {
+		geology = append(geology, strings.Split(scanner.Text(), ""))
+	}
+	if err := scanner.Err(); err != nil {
+		return nil, err
+	}
+
+	return geology, nil
+}

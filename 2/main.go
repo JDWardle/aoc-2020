432dd951d846097ac63362607021880abf1a7f52
Jesse Wardle <jesse.wardle@getweave.com>
2020-12-02 19:46:35 -0700

    Day 2 part 2

--- 2/main.go
+++ 2/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"bytes"
 	"flag"
 	"fmt"
 	"io"
@@ -17,6 +18,8 @@ type Policy struct {
 	Character string
 }
 
+// Valid returns true if the password contains at least Min number of Character
+// but no more then Max.
 func (p *Policy) Valid(password string) bool {
 	var n int
 	for _, r := range password {
@@ -25,6 +28,24 @@ func (p *Policy) Valid(password string) bool {
 		}
 	}
 	return n >= p.Min && n <= p.Max
+}
+
+type PolicyPart2 struct {
+	Pos       []int
+	Character string
+}
+
+// Valid returns true if the password contains the Character only once at two
+// positions.
+func (p *PolicyPart2) Valid(password string) bool {
+	pos1 := password[p.Pos[0]-1:p.Pos[0]] == p.Character
+	pos2 := password[p.Pos[1]-1:p.Pos[1]] == p.Character
+
+	if pos1 && pos2 {
+		return false
+	}
+
+	return pos1 || pos2
 }
 
 func main() {
@@ -37,12 +58,22 @@ func main() {
 	}
 	defer f.Close()
 
-	valid, err := readInput(f)
+	var buf bytes.Buffer
+	tee := io.TeeReader(f, &buf)
+
+	valid, err := readInput(tee)
 	if err != nil {
 		panic(err)
 	}
 
-	fmt.Println(valid)
+	fmt.Printf("Part one: %d\n", valid)
+
+	valid, err = readInputPart2(&buf)
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Printf("Part two: %d\n", valid)
 }
 
 func readInput(r io.Reader) (valid int, err error) {
@@ -82,6 +113,55 @@ func readInput(r io.Reader) (valid int, err error) {
 				valid++
 			}
 			mode = "min"
+		}
+	}
+
+	return
+}
+
+func readInputPart2(r io.Reader) (valid int, err error) {
+	var s scanner.Scanner
+	s.Init(r)
+
+	policy := &PolicyPart2{
+		Pos: make([]int, 2),
+	}
+
+	mode := "pos1"
+
+	for tok := s.Scan(); tok != scanner.EOF; tok = s.Scan() {
+		txt := s.TokenText()
+		if txt == "-" || txt == ":" {
+			continue
+		}
+
+		switch mode {
+		case "pos1":
+			n, err := strconv.Atoi(txt)
+			if err != nil {
+				return 0, err
+			}
+
+			policy.Pos[0] = n
+
+			mode = "pos2"
+		case "pos2":
+			n, err := strconv.Atoi(txt)
+			if err != nil {
+				return 0, err
+			}
+
+			policy.Pos[1] = n
+
+			mode = "char"
+		case "char":
+			policy.Character = txt
+			mode = "pass"
+		case "pass":
+			if policy.Valid(txt) {
+				valid++
+			}
+			mode = "pos1"
 		}
 	}
 

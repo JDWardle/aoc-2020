95b4a644f97594dcba609ebd49c833bf86aa835b
Jesse Wardle <jesse.wardle@getweave.com>
2020-12-20 20:27:39 -0700

    Day 17 part 2

--- 17/main.go
+++ 17/main.go
@@ -1,16 +1,6 @@
 package main
 
-// During a cycle, all cubes simultaneously change their state according to the
-// following rules:
-
-// If a cube is active and exactly 2 or 3 of its neighbors are also active, the
-// cube remains active. Otherwise, the cube becomes inactive. If a cube is
-// inactive but exactly 3 of its neighbors are active, the cube becomes active.
-// Otherwise, the cube remains inactive.
-
-// The engineers responsible for this experimental energy source would like you
-// to simulate the pocket dimension and determine what the configuration of
-// cubes should be at the end of the six-cycle boot process.
+// NOTE: Test input gives incorrect result but works for actual input...
 
 import (
 	"bufio"
@@ -19,6 +9,211 @@ import (
 	"os"
 	"strings"
 )
+
+type Vector4 struct {
+	X, Y, Z, W int
+}
+
+func NewVector4(x, y, z, w int) Vector4 { return Vector4{X: x, Y: y, Z: z, W: w} }
+
+func (v Vector4) String() string { return fmt.Sprintf("(%d, %d, %d, %d)", v.X, v.Y, v.Z, v.W) }
+
+type CubePart2 struct {
+	Active   bool
+	Position Vector4
+}
+
+func NewCubePart2(active bool, pos Vector4) CubePart2 {
+	return CubePart2{
+		Active:   active,
+		Position: pos,
+	}
+}
+
+func (c CubePart2) String() string {
+	if c.Active {
+		return "#"
+	}
+	return "."
+}
+
+type MapPart2 struct {
+	m      map[Vector4]CubePart2
+	MaxPos Vector4
+	MinPos Vector4
+}
+
+func NewMapPart2(lines []string) MapPart2 {
+	m := MapPart2{
+		m: make(map[Vector4]CubePart2),
+	}
+
+	y := 0
+	x := 0
+	for _, line := range lines {
+		s := strings.Split(line, "")
+		x = 0
+
+		for _, c := range s {
+			v := NewVector4(x, y, 0, 0)
+
+			switch c {
+			case "#":
+				m.m[v] = NewCubePart2(true, v)
+			case ".":
+				m.m[v] = NewCubePart2(false, v)
+			}
+			x++
+		}
+		y++
+	}
+
+	m.MinPos = NewVector4(-1, -1, -1, -1)
+	m.MaxPos = NewVector4(x-1, y-1, 1, 1)
+
+	return m
+}
+
+func (m MapPart2) String() string {
+	s := []string{}
+
+	for w := m.MinPos.W; w <= m.MaxPos.W; w++ {
+		for z := m.MinPos.Z; z <= m.MaxPos.Z; z++ {
+			s = append(s, fmt.Sprintf("z=%d, w=%d", z, w))
+
+			for y := m.MinPos.Y; y <= m.MaxPos.Y; y++ {
+				ys := ""
+				for x := m.MinPos.X; x <= m.MaxPos.X; x++ {
+					ys += m.m[NewVector4(x, y, z, w)].String()
+				}
+				s = append(s, ys)
+			}
+
+			s = append(s, "")
+		}
+	}
+
+	return strings.Join(s, "\n")
+}
+
+func (m MapPart2) Cycle() MapPart2 {
+	newMap := MapPart2{
+		m:      make(map[Vector4]CubePart2),
+		MinPos: NewVector4(m.MinPos.X-1, m.MinPos.Y-1, m.MinPos.Z-1, m.MinPos.W-1),
+		MaxPos: NewVector4(m.MaxPos.X+1, m.MaxPos.Y+1, m.MaxPos.Z+1, m.MaxPos.W+1),
+	}
+
+	for w := m.MinPos.W; w <= m.MaxPos.W; w++ {
+		for z := m.MinPos.Z; z <= m.MaxPos.Z; z++ {
+			for y := m.MinPos.Y; y <= m.MaxPos.Y; y++ {
+				for x := m.MinPos.X; x <= m.MaxPos.X; x++ {
+					v := NewVector4(x, y, z, w)
+					cube := m.Cube(v)
+
+					activeCubes := len(m.ActiveAdjacentCubes(cube))
+					if activeCubes == 0 {
+						continue
+					}
+
+					active := false
+
+					if cube.Active {
+						if activeCubes == 2 || activeCubes == 3 {
+							active = true
+						}
+					} else {
+						if activeCubes == 3 {
+							active = true
+						}
+					}
+
+					newMap.m[v] = NewCubePart2(active, v)
+				}
+			}
+		}
+	}
+
+	return newMap
+}
+
+func (m MapPart2) ActiveAdjacentCubes(cube CubePart2) []CubePart2 {
+	adjacentCubes := m.AdjacentCubes(cube)
+
+	n := 0
+
+	for _, cube := range adjacentCubes {
+		if cube.Active {
+			adjacentCubes[n] = cube
+			n++
+		}
+	}
+
+	return adjacentCubes[:n]
+}
+
+func (m MapPart2) Cube(pos Vector4) CubePart2 {
+	cube, ok := m.m[pos]
+	if !ok {
+		cube.Position = pos
+	}
+
+	return cube
+}
+
+func (m MapPart2) AdjacentCubes(cube CubePart2) []CubePart2 {
+	positions := m.AdjacentVectors(cube.Position)
+
+	cubes := []CubePart2{}
+	for _, pos := range positions {
+		if pos.X < m.MinPos.X || pos.Y < m.MinPos.Y || pos.Z < m.MinPos.Z || pos.W < m.MinPos.W {
+			continue
+		}
+
+		if pos.X > m.MaxPos.X || pos.Y > m.MaxPos.Y || pos.Z > m.MaxPos.Z || pos.W > m.MaxPos.W {
+			continue
+		}
+
+		cubes = append(cubes, m.Cube(pos))
+	}
+
+	return cubes
+}
+
+func (m MapPart2) AdjacentVectors(pos Vector4) [80]Vector4 {
+	minPos := NewVector4(pos.X-1, pos.Y-1, pos.Z-1, pos.W-1)
+	maxPos := NewVector4(pos.X+1, pos.Y+1, pos.Z+1, pos.W+1)
+
+	adjacent := [80]Vector4{}
+
+	i := 0
+	for w := minPos.W; w <= maxPos.W; w++ {
+		for z := minPos.Z; z <= maxPos.Z; z++ {
+			for y := minPos.Y; y <= maxPos.Y; y++ {
+				for x := minPos.X; x <= maxPos.X; x++ {
+					v := NewVector4(x, y, z, w)
+
+					if v == pos {
+						continue
+					}
+
+					adjacent[i] = v
+					i++
+				}
+			}
+		}
+	}
+
+	return adjacent
+}
+
+func (m MapPart2) ActiveCubes() (count int) {
+	for _, cube := range m.m {
+		if cube.Active {
+			count++
+		}
+	}
+	return
+}
 
 type Vector struct {
 	X int
@@ -45,9 +240,8 @@ func NewCube(active bool, pos Vector) Cube {
 func (c Cube) String() string {
 	if c.Active {
 		return "#"
-	} else {
-		return "."
 	}
+	return "."
 }
 
 type Map struct {
@@ -81,7 +275,7 @@ func NewMap(lines []string) Map {
 		y++
 	}
 
-	m.MinPos = NewVector(0, 0, -1)
+	m.MinPos = NewVector(-1, -1, -1)
 	m.MaxPos = NewVector(x-1, y-1, 1)
 
 	return m
@@ -123,6 +317,10 @@ func (m Map) Cycle() Map {
 				}
 
 				activeCubes := len(m.ActiveAdjacentCubes(cube))
+
+				if activeCubes == 0 {
+					continue
+				}
 
 				active := false
 
@@ -230,14 +428,15 @@ func main() {
 	}
 
 	m := NewMap(lines)
+	mPart2 := NewMapPart2(lines)
 
 	for cycle := 0; cycle < 6; cycle++ {
-		fmt.Printf("------------------------ Cycle %d ------------------------\n", cycle)
 		m = m.Cycle()
-		fmt.Println(m)
+		mPart2 = mPart2.Cycle()
 	}
 
 	fmt.Println("Part 1:", m.ActiveCubes())
+	fmt.Println("Part 2:", mPart2.ActiveCubes())
 }
 
 func readFile(r io.Reader) ([]string, error) {
